a0160345ed48dc592b11b73ba3851b448a7d8279
Stephan Hener <hener.stephan@gmail.com>
2016-12-22 12:40:19 +0100

    MOD optimize imports

--- tedbottompicker/src/main/java/gun0912/tedbottompicker/CameraPermissionActivity.java
+++ tedbottompicker/src/main/java/gun0912/tedbottompicker/CameraPermissionActivity.java
@@ -7,7 +7,6 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
-import android.os.PersistableBundle;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
